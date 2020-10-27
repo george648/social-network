f0eb10b17bcb2fe5baae3ed8b59bddaaa202900c
Geroge Balashevich <35461002238898@mail.ru>
2020-10-27 21:24:47 +0300

    refactoring component Dialog using map-methods

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,7 +7,7 @@ const DialogItem = (props) => {
   let path = '/dialogs/' + props.id;
   return ( 
     <div className={styles.dialog + ' ' + styles.active} >
-      <NavLink className={styles.dialogLink} to={path} >{props.name}</NavLink>
+      <NavLink className={styles.dialogLink} to={path}> {props.name} </NavLink>
     </div>
   )
 }
@@ -22,21 +22,34 @@ const Message = (props) => {
 
 
 const Dialogs = (props) => {
+  
+  let dialogData = [
+    {id:1, age:42, name: 'Varya'},
+    {id:2, name: 'Brother'},
+    {id:3, name: 'Mother'},
+    {id:4, name: 'Stas'},
+    {id:5, name: 'Friend'},
+  ];
+
+  let messageData = [
+    {id:1, message: 'hello'},
+    {id:2, message: 'its'},
+    {id:3, message: 'good'},
+    {id:4, message: 'fine '},
+    {id:5, message: 'message'},
+  ]
+
+  let diealogsElements  = dialogData.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>)
+  
+  let messagesElements = messageData.map(mes => <Message message={mes.message}/>)
+
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItem}>
-              <DialogItem id = '1' name = 'Stas'/>
-              <DialogItem id = '2' name = 'Varya'/>
-              <DialogItem id = '3' name = 'Mother'/>
-              <DialogItem id = '4' name = 'Brother'/>
-              <DialogItem id = '5' name = 'Friend'/>
+              {diealogsElements}
             </div>
            <div className={styles.messages}>
-              <Message message='Hello'/>
-              <Message message='how are you'/>
-              <Message message='is too easy to'/>
-              <Message message='programming on'/>
-              <Message message='React native'/>
+              {messagesElements}
           </div>
         </div>
     )
